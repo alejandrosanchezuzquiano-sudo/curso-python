dec060993976ad73420a98e2b8deb8412b25c433
alejandrosanchezuzquiano-sudo <alejandrosanchezuzquiano@gmail.com>
2026-02-08 18:48:32 +0100

    03_list_solutions

--- 02_flow_control/03_list.py
+++ 02_flow_control/03_list.py
@@ -72,10 +72,20 @@ print("Longitud de la lista", len(lista1))
 # mensaje = ["C", "o", "d", "i", "g", "o", " ", "s", "e", "c", "r", "e", "t", "o"]
 # Utilizando slicing y concatenación, crea una nueva lista que contenga solo el mensaje "secreto".
 
+mensaje = ["C", "o", "d", "i", "g", "o", " ", "s", "e", "c", "r", "e", "t", "o"]
+mensaje = mensaje[7:]
+print(mensaje)
+
 # Ejercicio 2: Intercambio de posiciones
 # Dada la siguiente lista:
 # numeros = [10, 20, 30, 40, 50]
 # Intercambia la primera y la última posición utilizando solo asignación por índice.
+
+numeros = [10, 20, 30, 40, 50]
+numeros1 = numeros[:]
+numeros1[0] = numeros[-1]
+numeros1[-1] = numeros[0]
+print(numeros1)
 
 # Ejercicio 3: El sándwich de listas
 # Dadas las siguientes listas:
@@ -84,16 +94,34 @@ print("Longitud de la lista", len(lista1))
 # pan_abajo = ["pan abajo"]
 # Crea una lista llamada sandwich que contenga el pan de arriba, los ingredientes y el pan de abajo, en ese orden.
 
+pan = ["pan arriba"]
+ingredientes = ["jamón", "queso", "tomate"]
+pan_abajo = ["pan abajo"]
+sandwich = pan + ingredientes + pan_abajo
+print(sandwich)
+
 # Ejercicio 4: Duplicando la lista
 # Dada una lista:
 # lista = [1, 2, 3]
 # Crea una nueva lista que contenga los elementos de la lista original duplicados.
 # Ejemplo: [1, 2, 3] -> [1, 2, 3, 1, 2, 3]
 
+lista = [1, 2, 3]
+lista1 = lista[:]+lista[:]
+print(lista1)
 # Ejercicio 5: Extrayendo el centro
 # Dada una lista con un número impar de elementos, extrae el elemento que se encuentra en el centro de la lista utilizando slicing.
 # Ejemplo: lista = [10, 20, 30, 40, 50] -> El centro es 30
 
+lista = [10, 20, 30, 40, 50]
+x = lista[int(float(len(lista)/2))]
+print(x)
+
 # Ejercicio 6: Reversa parcial
 # Dada una lista, invierte solo la primera mitad de la lista (utilizando slicing y concatenación).
 # Ejemplo: lista = [1, 2, 3, 4, 5, 6] -> Resultado: [3, 2, 1, 4, 5, 6]
+
+lista = [1, 2, 3, 4, 5, 6]
+lista1 = lista[int(float(len(lista)/2))-1::-1]
+lista2 = lista[int(float(len(lista)/2)):]
+print(lista1 + lista2)
